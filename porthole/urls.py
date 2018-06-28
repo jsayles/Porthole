d17a978d2316927d579348490eca4162a8c887b4
Jacob Sayles <jsayles@gmail.com>
2018-06-28 16:33:30 -0700

    added port table sorting

--- porthole/urls.py
+++ porthole/urls.py
@@ -5,13 +5,17 @@ from porthole import views
 
 app_name = 'porthole'
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('locations', views.location_list, name='locations'),
+    path('search/', views.search, name='search'),
+
+    path('locations/', views.location_list, name='locations'),
     path('location/<str:location>/', views.location_view, name='location'),
-    path('switches', views.switch_list, name='switches'),
+
+    path('switches/', views.switch_list, name='switches'),
     path('switch/<str:stack>/<int:unit>/', views.switch_view, name='switch'),
-    path('vlans', views.vlan_list, name='vlans'),
+    
+    path('vlans/', views.vlan_list, name='vlans'),
     path('vlan/<str:vlan>/', views.vlan_view, name='vlan'),
-    path('search', views.search, name='search'),
+
+    path('admin/', admin.site.urls),
 ]
--- porthole/views.py
+++ porthole/views.py
@@ -7,62 +7,24 @@ from django.shortcuts import get_object_or_404, render
 from porthole.models import Location, SwitchStack, Switch, VLAN, Port
 
 
-def home(request):
-    context = {}
-    return render(request, 'porthole/home.html', context)
+#########################################################################
+# Helper Utilities
+#########################################################################
 
-def location_list(request):
-    locations = (
-        (0, Location.objects.filter(floor=0)),
-        (1, Location.objects.filter(floor=1)),
-        (2, Location.objects.filter(floor=2)),
-    )
-    context = {
-        'locations': locations,
-    }
-    return render(request, 'porthole/location_list.html', context)
-
-def location_view(request, location):
-    location = get_object_or_404(Location, number=location)
-    ports = location.port_set.all().order_by('switch')
-    context = {
-        'location': location,
-        'ports': ports,
-    }
-    return render(request, 'porthole/location_view.html', context)
-
-def switch_list(request):
-    # A data closet is a location with switches in it
-    closets = Location.objects.data_closets()
-    context = {
-        'closets': closets
-    }
-    return render(request, 'porthole/switch_list.html', context)
-
-def switch_view(request, stack, unit):
-    switch = get_object_or_404(Switch, stack__name=stack, unit=unit)
-    ports = switch.port_set.all()
-    context = {
-        'switch': switch,
-        'ports': ports,
-    }
-    return render(request, 'porthole/switch_view.html', context)
-
-def vlan_list(request):
-    vlans = VLAN.objects.all().order_by('tag')
-    context = {
-        'vlans': vlans,
-    }
-    return render(request, 'porthole/vlan_list.html', context)
-
-def vlan_view(request, vlan):
-    vlan = get_object_or_404(VLAN, tag=vlan)
-    ports = vlan.port_set.all()
-    context = {
-        'vlan': vlan,
-        'ports': ports,
-    }
-    return render(request, 'porthole/vlan_view.html', context)
+def sort_ports(ports, order_by):
+    if not ports or not order_by:
+        return
+    if order_by.startswith('p'):
+        ports = ports.order_by('label')
+    elif order_by.startswith('l'):
+        ports = ports.order_by('location')
+    elif order_by.startswith('s'):
+        ports = ports.order_by('switch', 'switch_port')
+    elif order_by.startswith('v'):
+        ports = ports.order_by('vlan')
+    if len(order_by) == 2 and order_by[1] == '-':
+        ports = ports.reverse()
+    return ports
 
 def split_label(label):
     alphas = ""
@@ -73,6 +35,14 @@ def split_label(label):
         elif c.isdigit():
             digits += c
     return (alphas, digits)
+
+#########################################################################
+# Generic Views
+#########################################################################
+
+def home(request):
+    context = {}
+    return render(request, 'porthole/home.html', context)
 
 def search(request):
     closets = Location.objects.data_closets()
@@ -90,23 +60,94 @@ def search(request):
 
             if 'port_label' in request.POST:
                 port_label = request.POST['port_label']
-                alphas = ""
-                digits = ""
-                for c in port_label:
-                    if c.isalpha():
-                        alphas += c
-                    elif c.isdigit():
-                        digits += c
+                alphas, digits = split_label(port_label)
                 if alphas:
                     ports = ports.filter(label__istartswith=alphas)
                 if digits:
                     ports = ports.filter(label__contains=digits)
         except Exception as e:
             messages.add_message(request, messages.ERROR, f"Error performing search: {e} ")
+    order_by = request.GET.get('order_by', 'p')
+    ports = sort_ports(ports, order_by)
     context = {
         'closets': closets,
         'ports': ports,
+        'order_by': order_by,
         'q_closet': closet_number,
         'q_label': port_label,
     }
     return render(request, 'porthole/search.html', context)
+
+#########################################################################
+# Location Views
+#########################################################################
+
+def location_list(request):
+    locations = (
+        (0, Location.objects.filter(floor=0)),
+        (1, Location.objects.filter(floor=1)),
+        (2, Location.objects.filter(floor=2)),
+    )
+    context = {
+        'locations': locations,
+    }
+    return render(request, 'porthole/location_list.html', context)
+
+def location_view(request, location):
+    location = get_object_or_404(Location, number=location)
+    ports = location.port_set.all().order_by('switch')
+    order_by = request.GET.get('order_by', 's')
+    ports = sort_ports(ports, order_by)
+    context = {
+        'location': location,
+        'ports': ports,
+        'order_by': order_by,
+    }
+    return render(request, 'porthole/location_view.html', context)
+
+#########################################################################
+# Switch Views
+#########################################################################
+
+def switch_list(request):
+    # A data closet is a location with switches in it
+    closets = Location.objects.data_closets()
+    context = {
+        'closets': closets
+    }
+    return render(request, 'porthole/switch_list.html', context)
+
+def switch_view(request, stack, unit):
+    switch = get_object_or_404(Switch, stack__name=stack, unit=unit)
+    ports = switch.port_set.all()
+    order_by = request.GET.get('order_by', 's')
+    ports = sort_ports(ports, order_by)
+    context = {
+        'switch': switch,
+        'ports': ports,
+        'order_by': order_by,
+    }
+    return render(request, 'porthole/switch_view.html', context)
+
+#########################################################################
+# VLAN Views
+#########################################################################
+
+def vlan_list(request):
+    vlans = VLAN.objects.all().order_by('tag')
+    context = {
+        'vlans': vlans,
+    }
+    return render(request, 'porthole/vlan_list.html', context)
+
+def vlan_view(request, vlan):
+    vlan = get_object_or_404(VLAN, tag=vlan)
+    ports = vlan.port_set.all()
+    order_by = request.GET.get('order_by', 's')
+    ports = sort_ports(ports, order_by)
+    context = {
+        'vlan': vlan,
+        'ports': ports,
+        'order_by': order_by,
+    }
+    return render(request, 'porthole/vlan_view.html', context)
